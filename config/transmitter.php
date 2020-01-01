c0fa8f2936216ae288ad9012be6dbd0cbe47aee6
Al- Imran Ahmed <al.imran.cse@gmail.com>
2020-01-01 20:11:35 +0600

    Fixed: Unnacessary code removed

--- config/transmitter.php
+++ config/transmitter.php
@@ -13,9 +13,9 @@ return [
         'textvertising' => [
             "url" => env('TEXTVERTISING_URL', "https://www.textvertising.co.uk/_admin/api/send_sms.asp"),
             'sender' => env('TEXTVERTISING_SENDER', 'Media365'),
-            "username" => env('TEXTVERTISING_USERNAME', "FinCapOps"),
-            "password" => env('TEXTVERTISING_PASSWORD', "Summer123"),
-            'api_key' => env('TEXTVERTISING_API_KEY', "97219994867415920908427025955778694406349516375188"),
+            "username" => env('TEXTVERTISING_USERNAME'),
+            "password" => env('TEXTVERTISING_PASSWORD'),
+            'api_key' => env('TEXTVERTISING_API_KEY'),
         ]
     ]
 ];
