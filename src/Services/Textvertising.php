3deda6c12f5c80418009772840086b7e6284321c
mukti <mukti.arn@gmail.com>
2020-01-24 18:18:36 +0600

    sms error handle

--- src/Services/Textvertising.php
+++ src/Services/Textvertising.php
@@ -7,6 +7,19 @@ use GuzzleHttp\Client;
 
 class Textvertising implements SMS
 {
+    public static $statusMap = [
+        'FAILED LOGIN'      => 'Your API key is invalid or was omitted',
+        'ACCOUNT PROBLEM:(.*)'  => 'Your account is barred or another problem exists. [RESPONSE_MESSAGE] will be replaced by a specific error.',
+        'MESSAGE TOO LONG'  => 'The submitted message is longer than 160 characters',
+        'ONE OR MORE MOBILE NUMBERS IS INVALID ' => 'One or more of the numbers submitted is either too long, too short, is not a valid mobile number or does not start with 447… If the response style is XML it means that there were no valid numbers found at all.',
+        'MESSAGE CONTAINS INVALID CHARACTERS AT POSITION(.*)' => 'The message contains an invalid character that cannot be accepted by the network. Where applicable [RESPONSE_MESSAGE] will be replace by the position of character(s).',
+        'SMSID TOO LONG' => 'The sender ID is longer than 11 characters if Alphanumeric or 13 numbers if Numeric.',
+        'EXPIRY WRONG FORMAT' => 'No longer supported',
+        'NO NUMBERS' => 'Your submission contains no valid mobile numbers or the wrong delimiter was used to seperate them.',
+        'NUMBER BARRED' => 'The number you are submitting to has text stop or has been barred by the platform/network from receiving messages.',
+        'NOT ENOUGH FREE MESSAGES ' => 'You have run out of SMS credits messages and should top up your account.',
+    ];
+
     public function send(string $mobile, string $text)
     {
         $queryParams = $this->buildQuery($mobile, $text);
@@ -37,7 +50,16 @@ class Textvertising implements SMS
         $client = new Client();
         $response = $client->post($url, ['form_params' => $queryParams, 'headers' => $headers]);
 
-        if ($response) {
+        $content = $response->contents ?? '';
+        foreach (SMS::$statusMap as $statusPattern => $message) {
+            if( preg_match("/{$statusPattern}/i", $content, $matches) ) {
+                $responseMessage = $matches[1] ?? '';
+                $errorMessage = str_replace('[RESPONSE_MESSAGE]', $responseMessage, $message);
+                throw new \Exception("SMS sending failed to $mobile. Response: $content. Message: $errorMessage");
+            }
+        }
+
+        if ($content) {
             return true;
         }
 
